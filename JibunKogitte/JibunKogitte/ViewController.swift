956d3a563eeb2190913050f91a4fd0dbdd19af20
梶嶋 佐知子 <sachiko@kajishima.com>
2016-03-04 18:50:55 +0900

    初期コミット

---
+++ JibunKogitte/JibunKogitte/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JibunKogitte
+//
+//  Created by 梶嶋 佐知子 on 2016/03/04.
+//  Copyright © 2016年 KOMS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
