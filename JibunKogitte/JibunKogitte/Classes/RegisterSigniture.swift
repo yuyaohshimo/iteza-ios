3d2d6188b67c88afd948d1814576759f4d18b2d8
梶嶋 佐知子 <sachiko@kajishima.com>
2016-03-10 22:26:48 +0900

    サイン登録画面追加

---
+++ JibunKogitte/JibunKogitte/Classes/RegisterSigniture.swift
@@ -0,0 +1,15 @@
+//
+//  RegisterSigniture.swift
+//  JibunKogitte
+//
+//  Created by 梶嶋 佐知子 on 2016/03/10.
+//  Copyright © 2016年 KOMS. All rights reserved.
+//
+
+import UIKit
+
+class RegisterSigniture: BaseViewController {
+    
+    
+
+}
