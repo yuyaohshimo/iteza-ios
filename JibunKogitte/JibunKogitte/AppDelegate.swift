d66c53d670cac1cf583113fb08ce5c2e3f121a20
梶嶋 佐知子 <sachiko@kajishima.com>
2016-03-12 19:54:09 +0900

    IDいただくまでコメントアウト

--- JibunKogitte/JibunKogitte/AppDelegate.swift
+++ JibunKogitte/JibunKogitte/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         
         // プッシュ登録(Bluemix)
-        IMFClient.sharedInstance().initializeWithBackendRoute("add_your_applicationRoute_here", backendGUID: "add_your_applicationId_here");
+        //IMFClient.sharedInstance().initializeWithBackendRoute("add_your_applicationRoute_here", backendGUID: "add_your_applicationId_here");
         //Initialize client Push SDK for Swift
 
 
@@ -55,10 +55,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // MARK:APNSへデバイスが登録できた
     func application(application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: NSData) {
         
-        let push = IMFPushClient.sharedInstance()
-        push.registerDeviceToken(deviceToken) { (response, error) -> Void in
-            print("registerDeviceToken")
-        }
+        //let push = IMFPushClient.sharedInstance()
+        //push.registerDeviceToken(deviceToken) { (response, error) -> Void in
+        //    print("registerDeviceToken")
+        //}
         
     }
     
